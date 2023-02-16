52be7495b5053536e72448c25ff5a90b267f5020
andhika001 <andhikadwi980@gmail.com>
2023-02-16 20:43:30 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import router from './routes/router.js';
-import db from './config/database.js';
 
 const app = express();
 const port = 5000;
-
-// (async () => {
-//   await db.sync();
-// })();
 
 app.use(cors());
 app.use(express.json());
